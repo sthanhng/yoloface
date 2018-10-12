a442d74706f2cd51968bbc3e492a487ec4400298
ThanhNS <sthanhng@gmail.com>
2018-10-12 13:42:55 +0700

    create the structure of the project

---
+++ utils.py
@@ -0,0 +1,14 @@
+# *******************************************************************
+#
+# Author : Thanh Nguyen, 2018
+# Email  : sthanhng@gmail.com
+# Github : https://github.com/sthanhng
+#
+# BAP, AI Team
+# Face detection using the YOLOv3 algorithm
+#
+# Description : utils.py
+# This file contains the code of the parameters and help functions
+#
+# *******************************************************************
+
---
+++ yoloface.py
@@ -0,0 +1,14 @@
+# *******************************************************************
+#
+# Author : Thanh Nguyen, 2018
+# Email  : sthanhng@gmail.com
+# Github : https://github.com/sthanhng
+#
+# BAP, AI Team
+# Face detection using the YOLOv3 algorithm
+#
+# Description : yoloface.py
+# The main code of the Face detection using the YOLOv3 algorithm
+#
+# *******************************************************************
+
