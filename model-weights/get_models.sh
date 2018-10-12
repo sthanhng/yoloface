99c789ba86db5761607816c0cd6e5638e12d8f89
ThanhNS <sthanhng@gmail.com>
2018-10-12 17:39:22 +0700

    add the instruction to get pre-trained models

--- model-weights/get_models.sh
+++ model-weights/get_models.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O ./yolov3.cfg
+wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O ./coco.names
+
+# for face detection using the YOLOv3 algorithm, you navigate to the below link:
+# https://drive.google.com/file/d/1xYasjU52whXMLT5MtF7RCPQkV66993oR/view?usp=sharing
+# to download the weights of the trained model
+# put then it into the model-weights/ folder
