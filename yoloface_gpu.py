4aa74e9d86757a8a526af456b1125d5780288152
ThanhNS <sthanhng@gmail.com>
2018-10-17 15:04:28 +0700

    add defaults values: model, anchors, classess, score, iou

--- yoloface_gpu.py
+++ yoloface_gpu.py
@@ -7,15 +7,15 @@ from YOLO import YOLO
 #####################################################################
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='',
+    parser.add_argument('--model', type=str, default='model-weights/YOLO_Face.h5',
                         help='path to model weights file')
-    parser.add_argument('--anchors', type=str, default='',
+    parser.add_argument('--anchors', type=str, default='cfg/yolo_anchors.txt',
                         help='path to anchor definitions')
-    parser.add_argument('--classes', type=str, default='',
+    parser.add_argument('--classes', type=str, default='cfg/face_classes.txt',
                         help='path to class definitions')
-    parser.add_argument('--score', type=float, default='',
+    parser.add_argument('--score', type=float, default=0.5,
                         help='the score threshold')
-    parser.add_argument('--iou', type=float, default='',
+    parser.add_argument('--iou', type=float, default=0.45,
                         help='the iou threshold')
     parser.add_argument('--img-size', type=list, action='store',
                         default=(416, 416), help='input image size')
