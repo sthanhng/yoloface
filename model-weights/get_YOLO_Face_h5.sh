8bd160ec206a88ebcfc659cf85df2256a5f5c5ff
ThanhNS <sthanhng@gmail.com>
2018-11-18 15:15:34 +0700

    add script to get the YOLO_Face.h5 model

---
+++ model-weights/get_YOLO_Face_h5.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# To run the `yoloface_gpu.py` you download the pre-trained model `YOLO_Face.h5` as below link:
+# https://drive.google.com/file/d/1jSb9bp4Q90VD5Pcz2IyP8gkTjT26uQG5/view?usp=sharing
+# and put it in the model-weights folder.
